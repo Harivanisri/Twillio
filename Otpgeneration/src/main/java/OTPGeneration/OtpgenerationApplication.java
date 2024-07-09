20fc3eeae52760000816509be369c9008fce91f9
Harivanisri <sriharivani2001@gmail.com>
2024-07-09 16:27:12 +0530

    Twillo third party api

---
+++ Otpgeneration/src/main/java/OTPGeneration/OtpgenerationApplication.java
@@ -0,0 +1,13 @@
+package OTPGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpgenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtpgenerationApplication.class, args);
+	}
+
+}
---
+++ Otpgeneration/src/test/java/OTPGeneration/OtpgenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package OTPGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpgenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
